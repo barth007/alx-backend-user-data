23d79fc155dc190a4cd610a7f34af3601cc8f736
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-05 12:47:11 +0000

    added task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,34 @@ filtered_logger.py
 """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        filters values of incoming records
+        """
+
+        messages = filter_datum(
+                self.fields,
+                self.REDACTION,
+                record.getMessage(),
+                self.SEPARATOR
+            )
+        record.msg = messages
+        return super().format(record)
 
 
 def filter_datum(
