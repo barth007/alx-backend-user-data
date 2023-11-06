fb832aa6dd9c84c9ca98513ed0f336ddfe1bfef0
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-06 08:58:10 +0000

    added task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,3 +13,15 @@ def hash_password(password: str) -> bytes:
 
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(14))
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    checking if password and hashed_password are both
+    valid
+    """
+
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False
