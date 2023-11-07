5ce2081724f178524e151781b0006083031498ea
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-07 06:59:44 +0000

    task one done

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def error_unauthorized() -> str:
+    """GET /api/v1/unauthorized
+    Return:
+     - abort object
+    """
+    return abort(401)
