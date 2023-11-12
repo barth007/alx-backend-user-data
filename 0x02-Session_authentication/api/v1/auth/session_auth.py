e6ba0a19f64cb17218b0cea688b98aa85343fcd4
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-12 04:20:37 +0000

    added task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ Session Auth Module
 """
 from api.v1.auth.auth import Auth
 from flask import request, abort
+import uuid
 
 
 class SessionAuth(Auth):
@@ -11,4 +12,22 @@ class SessionAuth(Auth):
     Session Auth Class
     """
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session
+
+        argument
+            user_id: str
+        Return
+            returns a session
+        """
+
+        if user_id is None:
+            return None
+        elif not isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return self.user_id_by_session_id
