b54e8e5531ff3a4bfad32449bcfa7d4804a6cecb
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-12 04:22:46 +0000

    added task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,4 +30,4 @@ class SessionAuth(Auth):
         else:
             session_id = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
-            return self.user_id_by_session_id
+            return session_id
