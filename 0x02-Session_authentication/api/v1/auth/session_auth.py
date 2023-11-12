e7618b97d6b849433067f6ba08ba5bc8fa5328a7
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-11-12 02:36:57 +0000

    added task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Session Auth Module
+"""
+from api.v1.auth.auth import Auth
+from flask import request, abort
+
+
+class SessionAuth(Auth):
+    """
+    Session Auth Class
+    """
+
+
+    pass
